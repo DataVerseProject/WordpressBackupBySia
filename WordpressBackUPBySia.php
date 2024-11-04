942c43ec882e080d5a2fa3aa9c31329e5565c687
mercuregit <ngumbukafon@gmail.com>
2024-11-04 21:52:44 +0100

    create :  wordpress plugin header file for the WordpressBAckupBySia plugin

---
+++ WordpressBackUPBySia.php
@@ -0,0 +1,13 @@
+/*
+ * Plugin Name:       WordpressBackUpBySia
+ * Description:       Handle wordpress site back.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Ngum Buka Fon Nyuydze
+ * Author URI:        https://www.ngumbukafon.pro/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Domain Path:       /languages
+ */
